8cccb42d1f7d3c0c9aaac9095d950cddefd8a89c
Alasdair Hall <Alasdair Hall>
2017-01-19 16:05:37 +0000

    first commit

---
+++ src/physical_network/DataFrame.java
@@ -0,0 +1,184 @@
+/*
+ *  (c) K.Bryson, Dept. of Computer Science, UCL (2016)
+ *  
+ *  YOU MAY MODIFY THIS CLASS TO IMPLEMENT Stop & Wait ARQ PROTOCOL.
+ *  (You will submit this class to Moodle.)
+ *  
+ */
+
+package physical_network;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
+/**
+ * Encapsulates the data for a network 'data frame'.
+ * At the moment this just includes a payload byte array.
+ * This may need to be extended to include necessary header information.
+ * 
+ * @author kevin-b
+ *
+ */
+
+public class DataFrame {
+	
+	public final byte[] payload;
+	private int destination = 0;
+	private int source;
+	private int ack = 0;
+		
+	public DataFrame(String payload) {
+		this.payload = payload.getBytes();
+	}
+	
+	public DataFrame(String payload, int destination) {
+		this.payload = payload.getBytes();
+		this.destination = destination;
+	}
+
+	public DataFrame(byte[] payload) {
+		this.payload = payload;
+	}
+	
+	public DataFrame(byte[] payload, int destination) {
+		this.payload = payload;
+		this.destination = destination;
+	}
+	
+	public int getDestination() {
+		return destination;
+	}
+
+	public byte[] getPayload() {
+		return payload;
+	}
+
+	public String toString() {
+		return new String(payload);		
+	}
+
+	/*
+	 * A factory method that can be used to create a data frame
+	 * from an array of bytes that have been received.
+	 */
+	public static DataFrame createFromReceivedBytes(byte[] byteArray) {
+		int dest = byteArray[0];
+		int source = byteArray[1];
+		// CheckSum bytes 2 and 3
+		int ack = byteArray[4];
+		byte[] payload = Arrays.copyOfRange(byteArray, 5, byteArray.length);
+
+		DataFrame created = new DataFrame(payload, dest);
+		created.setSource(source);
+		if(ack == 1) created.makeAcknowledgement();
+		
+		return created;
+	}
+
+	public void setSource(int source) {
+		this.source = source;
+	}
+
+	public int getSource() {
+		return this.source;
+	}
+
+	public void makeAcknowledgement() {
+		this.ack = 1;
+	}
+
+	public boolean isAcknowledgement() {
+		return this.ack == 1;
+	}
+
+	private byte[] getHeader() {
+		byte[] tempHead = {
+				(byte) (destination & 0xFF),
+				(byte) (source & 0xFF),
+				(byte) (ack & 0xFF)
+		};
+
+		byte[] temp = new byte[tempHead.length + payload.length];
+
+		System.arraycopy(tempHead, 0, temp, 0, tempHead.length);
+		System.arraycopy(payload, 0, temp, tempHead.length, payload.length);
+
+		byte[] checksum = calcChecksum(temp);
+
+		byte[] header = {
+				(byte) (destination & 0xFF),
+				(byte) (source & 0xFF),
+				checksum[0],
+				checksum[1],
+				(byte) (ack & 0xFF)
+		};
+
+		return header;
+	}
+
+	public static byte[] calcChecksum(byte[] data) {
+		int checksum = 0;
+		int length = data.length;
+		int i = 0;
+
+		while(length > 1) {
+			int word = ((data[i] << 8) | (data[i+1] & 0xFF));
+			checksum += word;
+
+			length -= 2;
+			i += 2;
+
+			// Check for carry
+			if((checksum & 0xFFFF0000) > 0) {
+				checksum = checksum & 0xFFFF;
+				checksum += 1;
+			}
+		}
+
+		// In the case there is an odd number of bytes
+		if(length == 1) {
+			int word = ((data[i] << 8) & 0xFF00);
+			checksum += word;
+			if((checksum & 0xFFFF0000) > 0) {
+				checksum = checksum & 0xFFFF;
+				checksum += 1;
+			}
+		}
+		// Flip every bit
+		checksum = ~checksum;
+		checksum = checksum & 0xFFFF;
+
+		byte[] cs = new byte[2];
+
+		cs[0] = (byte)((checksum >> 8) & 0x00FF);
+		cs[1] = (byte) (checksum & 0x00FF);
+
+		return cs;
+	}
+
+	public static boolean confirmChecksum(byte[] cs) {
+		int check = ((cs[0] << 8) | cs[1]);
+		return check == 0;
+	}
+
+	/*
+         * This method should return the byte sequence of the transmitted bytes.
+         * At the moment it is just the payload data ... but extensions should
+         * include needed header information for the data frame.
+         * Note that this does not need sentinel or byte stuffing
+         * to be implemented since this is carried out as the data
+         * frame is transmitted and received.
+         */
+	public byte[] getTransmittedBytes() {
+		byte[] header = getHeader();
+		int length = payload.length + header.length;
+		byte[] transmittedBytes = new byte[length];
+
+		System.arraycopy(header, 0, transmittedBytes, 0, header.length);
+		System.arraycopy(payload, 0, transmittedBytes, header.length, payload.length);
+
+		return transmittedBytes;
+	}	
+	
+}
---
+++ src/physical_network/NetworkCard.java
@@ -0,0 +1,322 @@
+/*
+ *  (c) K.Bryson, Dept. of Computer Science, UCL (2016)
+ *  
+ *  YOU MAY MODIFY THIS CLASS TO IMPLEMENT Stop & Wait ARQ PROTOCOL.
+ *  (You will submit this class to Moodle.)
+ *
+ */
+
+package physical_network;
+
+import java.util.Arrays;
+import java.util.concurrent.*;
+
+
+/**
+ * 
+ * Represents a network card that can be attached to a particular wire.
+ * 
+ * It has only two key responsibilities:
+ * i) Allow the sending of data frames consisting of arrays of bytes using send() method.
+ * ii) Receives data frames into an input queue with a receive() method to access them.
+ *
+ * @author K. Bryson
+ */
+
+public class NetworkCard {
+    
+	// Wire pair that the network card is atatched to.
+    private final TwistedWirePair wire;
+
+    // Unique device number and name given to the network card.
+    private final int deviceNumber;
+    private final String deviceName;
+
+    // Default values for high, low and mid- voltages on the wire.
+    private final double HIGH_VOLTAGE = 2.5;
+    private final double LOW_VOLTAGE = -2.5;
+    
+    // Default value for a signal pulse width that should be used in milliseconds.
+    private final int PULSE_WIDTH = 200;
+    
+    // Default value for maximum payload size in bytes.
+    private final int MAX_PAYLOAD_SIZE = 1500;
+
+    // Default value for input & output queue sizes.
+    private final int QUEUE_SIZE = 5;
+
+    // Output queue for dataframes being transmitted.
+    private LinkedBlockingQueue<DataFrame> outputQueue = new LinkedBlockingQueue<DataFrame>(QUEUE_SIZE);
+    
+    // Input queue for dataframes being received.
+    private LinkedBlockingQueue<DataFrame> inputQueue  = new LinkedBlockingQueue<DataFrame>(QUEUE_SIZE);
+
+    // Transmitter thread.
+    private Thread txThread;
+    
+    // Receiver thread.
+    private Thread rxThread;
+
+	private boolean ackReceived = false;
+    
+    /**
+     * NetworkCard constructor.
+     * @param deviceName This provides the name of this device, i.e. "Network Card A".
+     * @param wire       This is the shared wire that this network card is connected to.
+     * @param listener   A data frame listener that should be informed when data frames are received.
+     *                   (May be set to 'null' if network card should not respond to data frames.)
+     */
+    public NetworkCard(int number, TwistedWirePair wire) {
+    	
+    	this.deviceNumber = number;
+    	this.deviceName = "NetCard" + number;
+    	this.wire = wire;
+    	
+    	txThread = this.new TXThread();
+    	rxThread = this.new RXThread();
+    }
+    
+    /*
+     * Initialize the network card.
+     */
+    public void init() {
+    	txThread.start();
+    	rxThread.start();
+    }
+    
+    
+    public void send(DataFrame data) throws InterruptedException {
+    	outputQueue.put(data);
+    }
+
+    public DataFrame receive() throws InterruptedException {
+    	DataFrame data = inputQueue.take();
+    	return data;
+    }
+
+
+    /*
+     * Private inner thread class that transmits data.
+     */
+    private class TXThread extends Thread {
+    	
+    	public void run() {
+    		
+    		try {
+	    		while (true) {
+	    			
+	    			// Blocks if nothing is in queue.
+	    			DataFrame frame = outputQueue.take();
+					// Set source number
+					frame.setSource(deviceNumber);
+	    			transmitFrame(frame);
+	    		}
+    		} catch (InterruptedException except) {
+    			System.out.println(deviceName + " Transmitter Thread Interrupted - terminated.");
+    		}
+    		
+    	}
+
+    	/**
+         * Tell the network card to send this data frame across the wire.
+         * NOTE - THIS METHOD ONLY RETURNS ONCE IT HAS TRANSMITTED THE DATA FRAME.
+         * 
+         * @param frame  Data frame to transmit across the network.
+         */
+        public void transmitFrame(DataFrame frame) throws InterruptedException {
+    		if (frame != null) {
+    			
+    			// Low voltage signal to get ready ...
+    			wire.setVoltage(deviceName, LOW_VOLTAGE);
+    			sleep(PULSE_WIDTH*4);
+
+    			byte[] payload = frame.getTransmittedBytes();
+    			
+    			// Send bytes in asynchronous style with 0.2 seconds gaps between them.
+    			for (int i = 0; i < payload.length; i++) {
+    				
+    	    		// Byte stuff if required.
+    	    		if (payload[i] == 0x7E || payload[i] == 0x7D)
+    	    			transmitByte((byte)0x7D);
+    	    		
+    	    		transmitByte(payload[i]);
+    			}
+    			
+    			// Append a 0x7E to terminate frame.
+        		transmitByte((byte)0x7E);
+
+				// Reset voltage
+				wire.setVoltage(deviceName, 0);
+
+				if(!frame.isAcknowledgement()) {
+					ackReceived = false;
+					System.out.println("Waiting for ACK");
+
+					long start = System.currentTimeMillis();
+
+					// Wait a minute
+					while(System.currentTimeMillis() - start < (60 * 1000)) {
+						if (ackReceived) {
+							System.out.println("Break, ack has been received: " + deviceName);
+							break;
+						}
+					}
+
+					if(!ackReceived) {
+						System.out.println(deviceName + " timeout. Retransmitting.");
+						transmitFrame(frame);
+					}
+				}
+
+    		}
+
+    		
+        }
+        
+    	private void transmitByte(byte value) throws InterruptedException {
+
+    		// Low voltage signal ...
+    		wire.setVoltage(deviceName, LOW_VOLTAGE);
+    		sleep(PULSE_WIDTH*4);
+
+    		// Set initial pulse for asynchronous transmission.
+    		wire.setVoltage(deviceName, HIGH_VOLTAGE);
+    		sleep(PULSE_WIDTH);
+    		
+    		// Go through bits in the value (big-endian bits first) and send pulses.
+    		
+            for (int bit = 0; bit < 8; bit++) {
+                if ((value & 0x80) == 0x80) {
+                    wire.setVoltage(deviceName, HIGH_VOLTAGE);
+                } else {
+                    wire.setVoltage(deviceName, LOW_VOLTAGE);
+                }
+                
+                // Shift value.
+                value <<= 1;  
+
+                sleep(PULSE_WIDTH);
+            }
+    	}
+    	
+    }
+    
+    /*
+     * Private inner thread class that receives data.
+     */    
+    private class RXThread extends Thread {
+    	
+    	public void run() {
+    		
+        	try {
+        		
+    			// Listen for data frames.
+        		
+	    		while (true) {
+
+	    			byte[] bytePayload = new byte[MAX_PAYLOAD_SIZE];
+	    			int bytePayloadIndex = 0;
+		    		byte receivedByte;
+//	        		do {
+//
+//	        			receivedByte = receiveByte();
+//
+//	        			System.out.println(deviceName + " RECEIVED BYTE = " + Integer.toHexString(receivedByte & 0xFF));
+//
+//	        			if ((receivedByte & 0xFF) != 0x7E) {
+//	            			// Unstuff if escaped.
+//		        			if (receivedByte == 0x7D) {
+//		        				receivedByte = receiveByte();
+//		        				System.out.println(deviceName + " ESCAPED RECEIVED BYTE = " + Integer.toHexString(receivedByte & 0xFF));
+//		        			}
+//
+//		        			bytePayload[bytePayloadIndex] = receivedByte;
+//		        			bytePayloadIndex++;
+//	        			}
+//
+//	        		} while ((receivedByte & 0xFF) != 0x7E);
+					// Timur's fix to prevent the break of the loop when a ~ is encountered in the received string
+					while (true) {
+						receivedByte = receiveByte();
+
+						if ((receivedByte & 0xFF) == 0x7E) {
+							break;
+						}
+
+						System.out.println(deviceName + " RECEIVED BYTE = " + Integer.toHexString(receivedByte & 0xFF));
+
+						// Unstuff if escaped.
+						if (receivedByte == 0x7D) {
+							receivedByte = receiveByte();
+							System.out.println(deviceName + " ESCAPED RECEIVED BYTE = " + Integer.toHexString(receivedByte & 0xFF));
+						}
+
+						bytePayload[bytePayloadIndex] = receivedByte;
+						bytePayloadIndex++;
+
+					}
+					DataFrame df = DataFrame.createFromReceivedBytes(Arrays.copyOfRange(bytePayload, 0, bytePayloadIndex));
+	        		// Block receiving data if queue full.
+
+					byte[] csum = DataFrame.calcChecksum(bytePayload);
+					if(DataFrame.confirmChecksum(csum)) {
+						System.out.println("Congrats, the data frame was not corrupted!");
+						if(df.getDestination() != deviceNumber) {
+							System.out.println(deviceName + " is discarding packet");
+						} else {
+							if(df.isAcknowledgement()) {
+								System.out.println("Setting ackReceived to true");
+								ackReceived = true;
+							} else {
+								// Send acknowledgement
+								DataFrame ack = new DataFrame("ACK", df.getSource());
+								ack.makeAcknowledgement();
+								System.out.println(deviceName + " sending ACK");
+								outputQueue.put(ack);
+							}
+							// Process the frame
+							inputQueue.put(df);
+						}
+					} else {
+						System.out.println("I'm afraid to say that your data frame became corrupted");
+					}
+
+	    		}
+
+            } catch (InterruptedException except) {
+                System.out.println(deviceName + " Interrupted: " + getName());
+            }
+    		
+    	}
+    	
+    	public byte receiveByte() throws InterruptedException {
+    		
+    		double thresholdVoltage = (LOW_VOLTAGE + 2.0 * HIGH_VOLTAGE)/3;
+    		byte value = 0;
+    		    		
+    		while (wire.getVoltage(deviceName) < thresholdVoltage) {
+    			sleep(PULSE_WIDTH/10);
+    		}
+    		
+    		// Sleep till middle of next pulse.
+    		sleep(PULSE_WIDTH + PULSE_WIDTH/2);
+    		
+    		// Use 8 next pulses for byte.
+    		for (int i = 0; i < 8; i++) {
+    			
+    			value *= 2;
+    		
+        		if (wire.getVoltage(deviceName) > thresholdVoltage) {
+        			value += 1;
+        		}
+        		
+        		sleep(PULSE_WIDTH);
+    		}
+    		
+    		return value;
+    	}
+    	
+    }
+
+    
+}
